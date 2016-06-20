2d29461318b4aaf6f1cf159ea74f60ed19e9159a
sa <sixsecondfiesta@gmail.com>
2016-06-20 17:19:12 -0400

    use typescript and transpile

--- index.ts
+++ index.ts
@@ -1,40 +1,60 @@
 declare var woyzeck : [{ speaker: string, lines: string }]
 declare function key(key : string, cb : () => void) : void
 
-const speaker = document.querySelector('#speaker')
-const line = document.querySelector('#line')
+function randomInArray<T>(arr : T[]) : T { return arr[Math.floor(Math.random() * arr.length)] }
+
+let allVoices : SpeechSynthesisVoice[]
+let candidateVoices : SpeechSynthesisVoice[]
+let usedVoices : SpeechSynthesisVoice[]
+
+const charactersToVoices : { [character : string] : SpeechSynthesisVoice } = {}
+
+const speakerContainer = document.querySelector('#speaker')
+const lineContainer = document.querySelector('#line')
 
 let i = 0
+function doLine() : void {
+  const { speaker, lines } = woyzeck[i]
 
-function doLine() {
-  const dialog = woyzeck[i]
+  speakerContainer.textContent = speaker
+  lineContainer.textContent = lines
 
-  speaker.textContent = dialog.speaker
-  line.textContent = dialog.lines
+  let voice : SpeechSynthesisVoice
+  if(speaker in charactersToVoices) {
+    voice = charactersToVoices[speaker]
+  }
+  else {
+    if(candidateVoices.length > 0) {
+      voice = candidateVoices.pop()
+      usedVoices.push(voice)
+    }
+    else {
+      voice = randomInArray(usedVoices)
+    }
+    charactersToVoices[speaker] = voice
+  }
+
+  const utterance = new SpeechSynthesisUtterance(lines)
+  utterance.voice = voice
+  // utterance.pitch = pitch.value
+  // utterance.rate = rate.value
+  utterance.onend = () => setTimeout(() => {
+    i += 1
+    doLine()
+  }, 300)
+
+  speechSynthesis.speak(utterance)
 }
-
-doLine()
 
 key('right', () => { i += 1; if(i === woyzeck.length) { i = 0 } doLine() })
 key('left', () => { i -= 1; if(i < 0) { i = woyzeck.length - 1 } doLine() })
 
-
-let voices
 function populateVoiceList() {
-  voices = speechSynthesis.getVoices()
-
-  console.log(voices)
-  for(const v of voices) {
-    console.log(v.name)
-    // var option = document.createElement('option');
-    // option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-    // if(voices[i].default) {
-    //   option.textContent += ' -- DEFAULT';
-    // }
-
-    // option.setAttribute('data-lang', voices[i].lang);
-    // option.setAttribute('data-name', voices[i].name);
-    // voiceSelect.appendChild(option);
+  allVoices = speechSynthesis.getVoices()
+  candidateVoices = allVoices.filter(v => v.name.startsWith('english'))
+  usedVoices = []
+  if(candidateVoices.length > 0) {
+    doLine()
   }
 }
 
@@ -42,20 +62,3 @@ populateVoiceList()
 if(speechSynthesis.onvoiceschanged !== undefined) {
   speechSynthesis.onvoiceschanged = () => { console.log('changed!'); populateVoiceList() }
 }
-
-// inputForm.onsubmit = function(event) {
-//   event.preventDefault();
-
-//   var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
-//   var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
-//   for(i = 0; i < voices.length ; i++) {
-//     if(voices[i].name === selectedOption) {
-//       utterThis.voice = voices[i];
-//     }
-//   }
-//   utterThis.pitch = pitch.value;
-//   utterThis.rate = rate.value;
-//   synth.speak(utterThis);
-
-//   inputTxt.blur();
-// }
