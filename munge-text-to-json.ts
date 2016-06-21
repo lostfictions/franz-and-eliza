cd4f946f1e1c32b7497ac1dc0c3a177c42fb9796
sa <sixsecondfiesta@gmail.com>
2016-06-21 17:25:46 -0400

    works!

--- munge-text-to-json.ts
+++ munge-text-to-json.ts
@@ -2,27 +2,128 @@ import * as fs from 'fs'
 
 const text = fs.readFileSync('intermediate/woyzeck-cleaned.txt').toString()
 
+type Scene = {
+  name: string
+  setting?: string
+  note?: string
+  outline: OutlineElement[]
+}
+
+type OutlineElement = Dialog | StageDirection
+
+type Dialog = {
+  speaker: string
+  linesAndDirections: string[]
+}
+
+type StageDirection = string
+
+
+
+const terminators = ['.', '!', '/', ')']
+
 const munged = text
   .split('\n')
   .reduce((p, c) => {
     const line = c.trim()
+
+    //If line is empty, continue.
     if(line.length === 0) {
-      //If line is empty, continue
       return p
     }
 
+    console.log(line)
+
+    const currentScene = p[p.length - 1]
+
     const tokens = line.split(' ')
-    if(tokens[0].endsWith(':')) {
-      //We have a new line of dialog.
+
+    //If the first token is a '#', it's a new scene.
+    if(tokens[0] === '#') {
       p.push({
-        speaker: tokens[0].slice(0,-1),
-        lines: tokens.slice(1).join(' ')
+        name: tokens.slice(1).join(' '),
+        outline: []
       })
       return p
     }
-    //Append existing line.
-    p[p.length - 1].lines = p[p.length - 1].lines + ' ' + line
+
+    //If it's a '##', it's a scene setting.
+    if(tokens[0] === '##') {
+      currentScene.setting = tokens.slice(1).join(' ')
+      return p
+    }
+
+    //If it's a '##', it's a scene note.
+    if(tokens[0] === '###') {
+      currentScene.note = tokens.slice(1).join(' ')
+      return p
+    }
+
+    //If the first token ends with a ':', we have a new line of dialog.
+    if(tokens[0].endsWith(':')) {
+      //Split up by stage directions and sentences.
+      const tokensMinusSpeaker = tokens.slice(1)
+
+      const linesAndDirections = parseLine(tokensMinusSpeaker)
+
+      currentScene.outline.push({
+        speaker: tokens[0].slice(0, -1),
+        linesAndDirections: linesAndDirections
+      })
+      return p
+    }
+    //HACK: if the second token ends with a ':', do the same thing. Some names are two words long, but no more.
+    //Brittle, but good enough.
+    if(tokens.length > 1 && tokens[1].endsWith(':')) {
+      //Split up by stage directions and sentences.
+      const tokensMinusSpeaker = tokens.slice(2)
+
+      const linesAndDirections = parseLine(tokensMinusSpeaker)
+
+      currentScene.outline.push({
+        speaker: tokens.slice(0, 2).join(' ').slice(0, -1),
+        linesAndDirections: linesAndDirections
+      })
+      return p
+    }
+
+    //If the line starts with a '(', it's a stage direction.
+    if(line.startsWith('(')) {
+      currentScene.outline.push(line)
+    }
+
+    //Otherwise, append the text to the existing speaker's lines or the existing stage direction.
+    if(typeof currentScene.outline[currentScene.outline.length - 1] !== 'string') {
+      const d = currentScene.outline[currentScene.outline.length - 1] as Dialog
+      d.linesAndDirections = d.linesAndDirections.concat(parseLine(tokens))
+    }
+    else {
+      //The last element in the outline was a stage direction
+      const sd = currentScene.outline[currentScene.outline.length - 1] as StageDirection
+      currentScene.outline[currentScene.outline.length - 1] = sd + line
+    }
     return p
-  }, [])
+  }, [] as Scene[])
 
 fs.writeFileSync('public/munged_dialog.js', 'window.woyzeck = ' + JSON.stringify(munged, undefined, 2))
+
+function parseLine(tokens : string[]) : string[] {
+    let linesAndDirections = [] as string[]
+
+    let lineOrDirectionTokens = [] as string[]
+    while(tokens.length > 0) {
+      const t = tokens.shift()
+      lineOrDirectionTokens.push(t)
+      if(terminators.some(term => t.endsWith(term))) {
+        //This is the end of a line/direction; push it to the list of lines/directions and start a new one
+        linesAndDirections.push(lineOrDirectionTokens.join(' '))
+        lineOrDirectionTokens = []
+      }
+    }
+    //If we have any tokens left, push them too.
+    if(lineOrDirectionTokens.length > 0) {
+      linesAndDirections.push(lineOrDirectionTokens.join(' '))
+    }
+
+    return linesAndDirections
+}
