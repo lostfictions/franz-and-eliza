3649ace084afd0595b8cf585311458f4f1e9f1d5
sa <sixsecondfiesta@gmail.com>
2016-06-21 20:40:31 -0400

    working on the actual thing now!

--- index.ts
+++ index.ts
@@ -1,4 +1,20 @@
-declare var woyzeck : [{ speaker: string, lines: string }]
+type Scene = {
+  name: string
+  setting?: string
+  note?: string
+  outline: OutlineElement[]
+}
+
+type OutlineElement = Dialog | StageDirection
+
+type Dialog = {
+  speaker: string
+  linesAndDirections: string[]
+}
+
+type StageDirection = string
+
+declare var woyzeck : Scene[]
 declare function key(key : string, cb : () => void) : void
 
 function randomInArray<T>(arr : T[]) : T { return arr[Math.floor(Math.random() * arr.length)] }
@@ -9,56 +25,161 @@ let usedVoices : SpeechSynthesisVoice[]
 
 const charactersToVoices : { [character : string] : SpeechSynthesisVoice } = {}
 
+const characters = new Set<string>()
+woyzeck.forEach(scene => {
+  const ds = scene.outline.filter(el => typeof el !== 'string') as Dialog[]
+  ds.forEach(el => characters.add(el.speaker))
+})
+
+const preferredVoices = {
+  'Woyzeck': undefined,
+  'Marie': undefined,
+  'Doctor': undefined,
+  'Captain': undefined,
+  'Andres': undefined,
+  'Margaret': undefined,
+  'Drum Major': undefined
+}
+
+const firefoxCandidateVoices = [
+  'english',
+  'english-us',
+  'english_rp',
+  'english-north',
+  'english_wmids',
+  'en-scottish',
+  'en-westindies',
+  'german',
+  'default'
+]
+
+const firefoxOtherVoices = [
+  'Farsi',
+  'Farsi-Pinglish',
+  'Mandarin',
+  'afrikaans',
+  'albanian',
+  'aragonese',
+  'armenian',
+  'armenian-west',
+  'bosnian',
+  'brazil',
+  'bulgarian',
+  'cantonese',
+  'catalan',
+  'croatian',
+  'czech',
+  'danish',
+  'dutch',
+  'esperanto',
+  'estonian',
+  'finnish',
+  'french',
+  'french-Belgium',
+  'georgian',
+  'greek',
+  'greek-ancient',
+  'hindi',
+  'hungarian',
+  'icelandic',
+  'indonesian',
+  'irish-gaeilge',
+  'italian',
+  'kannada',
+  'kurdish',
+  'latin',
+  'latvian',
+  'lithuanian',
+  'lojban',
+  'macedonian',
+  'malay',
+  'malayalam',
+  'nepali',
+  'norwegian',
+  'polish',
+  'portugal',
+  'punjabi',
+  'romanian',
+  'russian',
+  'serbian',
+  'slovak',
+  'spanish',
+  'spanish-latin-am',
+  'swahili-test',
+  'swedish',
+  'tamil',
+  'turkish',
+  'vietnam',
+  'vietnam_hue',
+  'vietnam_sgn',
+  'welsh'
+]
+
+
 const speakerContainer = document.querySelector('#speaker')
 const lineContainer = document.querySelector('#line')
 
-let i = 0
-function doLine() : void {
-  const { speaker, lines } = woyzeck[i]
+let sceneIndex = 0
+let outlineIndex = 0
+let lineIndex = 0
+function doPlay() : void {
+  const { outline } = woyzeck[sceneIndex]
+
+  const currentEl = outline[outlineIndex]
+  if(typeof currentEl === 'string') {
+    lineContainer.textContent = currentEl
+    return
+  }
+
+  const { speaker, linesAndDirections } = currentEl as Dialog
 
   speakerContainer.textContent = speaker
-  lineContainer.textContent = lines
+  lineContainer.textContent = linesAndDirections[lineIndex]
 
-  let voice : SpeechSynthesisVoice
-  if(speaker in charactersToVoices) {
-    voice = charactersToVoices[speaker]
-  }
-  else {
-    if(candidateVoices.length > 0) {
-      voice = candidateVoices.pop()
-      usedVoices.push(voice)
-    }
-    else {
-      voice = randomInArray(usedVoices)
-    }
-    charactersToVoices[speaker] = voice
-  }
+  // let voice : SpeechSynthesisVoice
+  // if(speaker in charactersToVoices) {
+  //   voice = charactersToVoices[speaker]
+  // }
+  // else {
+  //   if(candidateVoices.length > 0) {
+  //     voice = candidateVoices.pop()
+  //     usedVoices.push(voice)
+  //   }
+  //   else {
+  //     voice = randomInArray(usedVoices)
+  //   }
+  //   charactersToVoices[speaker] = voice
+  // }
 
-  const utterance = new SpeechSynthesisUtterance(lines)
-  utterance.voice = voice
-  // utterance.pitch = pitch.value
-  // utterance.rate = rate.value
-  utterance.onend = () => setTimeout(() => {
-    i += 1
-    doLine()
-  }, 300)
+  // const utterance = new SpeechSynthesisUtterance(lines)
+  // utterance.voice = voice
+  // // utterance.pitch = pitch.value
+  // // utterance.rate = rate.value
+  // utterance.onend = () => setTimeout(() => {
+  //   i += 1
+  //   doLine()
+  // }, 300)
 
-  speechSynthesis.speak(utterance)
+  // speechSynthesis.speak(utterance)
 }
 
-key('right', () => { i += 1; if(i === woyzeck.length) { i = 0 } doLine() })
-key('left', () => { i -= 1; if(i < 0) { i = woyzeck.length - 1 } doLine() })
+// key('right', () => { i += 1; if(i === woyzeck.length) { i = 0 } doPlay() })
+// key('left', () => { i -= 1; if(i < 0) { i = woyzeck.length - 1 } doPlay() })
 
 function populateVoiceList() : void {
   allVoices = speechSynthesis.getVoices()
+  console.log(allVoices.map(v => v.name).sort().join(', '))
   candidateVoices = allVoices.filter(v => v.name.startsWith('english'))
   usedVoices = []
   if(candidateVoices.length > 0) {
-    doLine()
+    doPlay()
   }
 }
 
+// for the speech synthesis api, we have to first request the voice list,
+// get back a probably-empty list, and then wait for the real list to be
+// returned and call our function again. amazing, i know. 
 populateVoiceList()
 if(speechSynthesis.onvoiceschanged !== undefined) {
-  speechSynthesis.onvoiceschanged = () => { console.log('changed!'); populateVoiceList() }
+  speechSynthesis.onvoiceschanged = () => { populateVoiceList() }
 }
--- public/index.js
+++ public/index.js
@@ -3,52 +3,146 @@ var allVoices;
 var candidateVoices;
 var usedVoices;
 var charactersToVoices = {};
+var characters = new Set();
+woyzeck.forEach(function (scene) {
+    var ds = scene.outline.filter(function (el) { return typeof el !== 'string'; });
+    ds.forEach(function (el) { return characters.add(el.speaker); });
+});
+var preferredVoices = {
+    'Woyzeck': undefined,
+    'Marie': undefined,
+    'Doctor': undefined,
+    'Captain': undefined,
+    'Andres': undefined,
+    'Margaret': undefined,
+    'Drum Major': undefined
+};
+var firefoxCandidateVoices = [
+    'english',
+    'english-us',
+    'english_rp',
+    'english-north',
+    'english_wmids',
+    'en-scottish',
+    'en-westindies',
+    'german',
+    'default'
+];
+var firefoxOtherVoices = [
+    'Farsi',
+    'Farsi-Pinglish',
+    'Mandarin',
+    'afrikaans',
+    'albanian',
+    'aragonese',
+    'armenian',
+    'armenian-west',
+    'bosnian',
+    'brazil',
+    'bulgarian',
+    'cantonese',
+    'catalan',
+    'croatian',
+    'czech',
+    'danish',
+    'dutch',
+    'esperanto',
+    'estonian',
+    'finnish',
+    'french',
+    'french-Belgium',
+    'georgian',
+    'greek',
+    'greek-ancient',
+    'hindi',
+    'hungarian',
+    'icelandic',
+    'indonesian',
+    'irish-gaeilge',
+    'italian',
+    'kannada',
+    'kurdish',
+    'latin',
+    'latvian',
+    'lithuanian',
+    'lojban',
+    'macedonian',
+    'malay',
+    'malayalam',
+    'nepali',
+    'norwegian',
+    'polish',
+    'portugal',
+    'punjabi',
+    'romanian',
+    'russian',
+    'serbian',
+    'slovak',
+    'spanish',
+    'spanish-latin-am',
+    'swahili-test',
+    'swedish',
+    'tamil',
+    'turkish',
+    'vietnam',
+    'vietnam_hue',
+    'vietnam_sgn',
+    'welsh'
+];
 var speakerContainer = document.querySelector('#speaker');
 var lineContainer = document.querySelector('#line');
-var i = 0;
-function doLine() {
-    var _a = woyzeck[i], speaker = _a.speaker, lines = _a.lines;
+var sceneIndex = 0;
+var outlineIndex = 0;
+var lineIndex = 0;
+function doPlay() {
+    var outline = woyzeck[sceneIndex].outline;
+    var currentEl = outline[outlineIndex];
+    if (typeof currentEl === 'string') {
+        lineContainer.textContent = currentEl;
+        return;
+    }
+    var _a = currentEl, speaker = _a.speaker, linesAndDirections = _a.linesAndDirections;
     speakerContainer.textContent = speaker;
-    lineContainer.textContent = lines;
-    var voice;
-    if (speaker in charactersToVoices) {
-        voice = charactersToVoices[speaker];
-    }
-    else {
-        if (candidateVoices.length > 0) {
-            voice = candidateVoices.pop();
-            usedVoices.push(voice);
-        }
-        else {
-            voice = randomInArray(usedVoices);
-        }
-        charactersToVoices[speaker] = voice;
-    }
-    var utterance = new SpeechSynthesisUtterance(lines);
-    utterance.voice = voice;
-    // utterance.pitch = pitch.value
-    // utterance.rate = rate.value
-    utterance.onend = function () { return setTimeout(function () {
-        i += 1;
-        doLine();
-    }, 300); };
-    speechSynthesis.speak(utterance);
+    lineContainer.textContent = linesAndDirections[lineIndex];
+    // let voice : SpeechSynthesisVoice
+    // if(speaker in charactersToVoices) {
+    //   voice = charactersToVoices[speaker]
+    // }
+    // else {
+    //   if(candidateVoices.length > 0) {
+    //     voice = candidateVoices.pop()
+    //     usedVoices.push(voice)
+    //   }
+    //   else {
+    //     voice = randomInArray(usedVoices)
+    //   }
+    //   charactersToVoices[speaker] = voice
+    // }
+    // const utterance = new SpeechSynthesisUtterance(lines)
+    // utterance.voice = voice
+    // // utterance.pitch = pitch.value
+    // // utterance.rate = rate.value
+    // utterance.onend = () => setTimeout(() => {
+    //   i += 1
+    //   doLine()
+    // }, 300)
+    // speechSynthesis.speak(utterance)
 }
-key('right', function () { i += 1; if (i === woyzeck.length) {
-    i = 0;
-} doLine(); });
-key('left', function () { i -= 1; if (i < 0) {
-    i = woyzeck.length - 1;
-} doLine(); });
+// key('right', () => { i += 1; if(i === woyzeck.length) { i = 0 } doPlay() })
+// key('left', () => { i -= 1; if(i < 0) { i = woyzeck.length - 1 } doPlay() })
 function populateVoiceList() {
     allVoices = speechSynthesis.getVoices();
+    console.log(allVoices.map(function (v) { return v.name; }).sort().join(', '));
     candidateVoices = allVoices.filter(function (v) { return v.name.startsWith('english'); });
     usedVoices = [];
     if (candidateVoices.length > 0) {
-        doLine();
+        doPlay();
     }
 }
+// for the speech synthesis api, we have to first request the voice list,
+// get back a probably-empty list, and then wait for the real list to be
+// returned and call our function again. amazing, i know. 
 populateVoiceList();
 if (speechSynthesis.onvoiceschanged !== undefined) {
-    speechSynthesis.onvoiceschanged = function () { console.log('changed!'); populateVoiceList(); };
+    speechSynthesis.onvoiceschanged = function () { populateVoiceList(); };
 }
