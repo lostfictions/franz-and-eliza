332a02c0be6616c145e6b8f1861c9f0bf63cfcd9
sa <sixsecondfiesta@gmail.com>
2016-06-22 13:20:24 -0400

    advancing logic, typography

--- index.ts
+++ index.ts
@@ -14,6 +14,26 @@ type Dialog = {
 
 type StageDirection = string
 
+type VoicePreferences = { [character : string] : [CharacterSpeech] }
+type CharacterSpeech = { voice: CandidateVoice, pitch: number, rate: number }
+type CandidateVoice =
+  'english' | 'english-us' | 'english_rp' | 'english-north' | 'english_wmids' |
+  'en-scottish' | 'en-westindies' | 'german' | 'default'
+
+/*
+type FirefoxVoices = 'english' | 'english-us' | 'english_rp' | 'english-north' | 'english_wmids' |
+  'en-scottish' | 'en-westindies' | 'german' | 'default' | 'Farsi' | 'Farsi-Pinglish' | 'Mandarin' |
+  'afrikaans' | 'albanian' | 'aragonese' | 'armenian' | 'armenian-west' | 'bosnian' |
+  'brazil' | 'bulgarian' | 'cantonese' | 'catalan' | 'croatian' | 'czech' | 'danish' |
+  'dutch' | 'esperanto' | 'estonian' | 'finnish' | 'french' | 'french-Belgium' |
+  'georgian' | 'greek' | 'greek-ancient' | 'hindi' | 'hungarian' | 'icelandic' |
+  'indonesian' | 'irish-gaeilge' | 'italian' | 'kannada' | 'kurdish' | 'latin' |
+  'latvian' | 'lithuanian' | 'lojban' | 'macedonian' | 'malay' | 'malayalam' |
+  'nepali' | 'norwegian' | 'polish' | 'portugal' | 'punjabi' | 'romanian' | 'russian' |
+  'serbian' | 'slovak' | 'spanish' | 'spanish-latin-am' | 'swahili-test' | 'swedish' |
+  'tamil' | 'turkish' | 'vietnam' | 'vietnam_hue' | 'vietnam_sgn' | 'welsh'
+*/
+
 declare var woyzeck : Scene[]
 declare function key(key : string, cb : () => void) : void
 
@@ -31,110 +51,29 @@ woyzeck.forEach(scene => {
   ds.forEach(el => characters.add(el.speaker))
 })
 
-const preferredVoices = {
-  'Woyzeck': undefined,
-  'Marie': undefined,
-  'Doctor': undefined,
-  'Captain': undefined,
-  'Andres': undefined,
-  'Margaret': undefined,
-  'Drum Major': undefined
+const preferredVoices : VoicePreferences = {
+  'Woyzeck': [{ voice: 'english-us', pitch: 1, rate: 1 }],
+  'Marie': [{ voice: 'english', pitch: 1.25, rate: 1 }],
+  'Doctor': [{ voice: 'english_rp', pitch: 0.9, rate: 0.95 }],
+  'Captain': [{ voice: 'en-scottish', pitch: 1, rate: 1 }],
+  'Andres': [{ voice: 'english-north', pitch: 1, rate: 1 }],
+  'Margaret': [{ voice: 'en-westindies', pitch: 1, rate: 1 }],
+  'Drum Major': [{ voice: 'english_wmids', pitch: 1, rate: 1 }]
 }
 
-const firefoxCandidateVoices = [
-  'english',
-  'english-us',
-  'english_rp',
-  'english-north',
-  'english_wmids',
-  'en-scottish',
-  'en-westindies',
-  'german',
-  'default'
-]
-
-const firefoxOtherVoices = [
-  'Farsi',
-  'Farsi-Pinglish',
-  'Mandarin',
-  'afrikaans',
-  'albanian',
-  'aragonese',
-  'armenian',
-  'armenian-west',
-  'bosnian',
-  'brazil',
-  'bulgarian',
-  'cantonese',
-  'catalan',
-  'croatian',
-  'czech',
-  'danish',
-  'dutch',
-  'esperanto',
-  'estonian',
-  'finnish',
-  'french',
-  'french-Belgium',
-  'georgian',
-  'greek',
-  'greek-ancient',
-  'hindi',
-  'hungarian',
-  'icelandic',
-  'indonesian',
-  'irish-gaeilge',
-  'italian',
-  'kannada',
-  'kurdish',
-  'latin',
-  'latvian',
-  'lithuanian',
-  'lojban',
-  'macedonian',
-  'malay',
-  'malayalam',
-  'nepali',
-  'norwegian',
-  'polish',
-  'portugal',
-  'punjabi',
-  'romanian',
-  'russian',
-  'serbian',
-  'slovak',
-  'spanish',
-  'spanish-latin-am',
-  'swahili-test',
-  'swedish',
-  'tamil',
-  'turkish',
-  'vietnam',
-  'vietnam_hue',
-  'vietnam_sgn',
-  'welsh'
-]
-
-
-const speakerContainer = document.querySelector('#speaker')
-const lineContainer = document.querySelector('#line')
+const sceneTitleContainer = document.querySelector('#scene-title') as HTMLElement
+const speakerContainer = document.querySelector('#speaker') as HTMLElement
+const lineContainer = document.querySelector('#line') as HTMLElement
+sceneTitleContainer.style.display = 'none'
+speakerContainer.style.display = 'none'
+lineContainer.style.display = 'none'
 
 let sceneIndex = 0
+let sceneInitialized = false
 let outlineIndex = 0
 let lineIndex = 0
 function doPlay() : void {
-  const { outline } = woyzeck[sceneIndex]
-
-  const currentEl = outline[outlineIndex]
-  if(typeof currentEl === 'string') {
-    lineContainer.textContent = currentEl
-    return
-  }
-
-  const { speaker, linesAndDirections } = currentEl as Dialog
-
-  speakerContainer.textContent = speaker
-  lineContainer.textContent = linesAndDirections[lineIndex]
+  renderCurrent()
 
   // let voice : SpeechSynthesisVoice
   // if(speaker in charactersToVoices) {
@@ -163,13 +102,88 @@ function doPlay() : void {
   // speechSynthesis.speak(utterance)
 }
 
-// key('right', () => { i += 1; if(i === woyzeck.length) { i = 0 } doPlay() })
-// key('left', () => { i -= 1; if(i < 0) { i = woyzeck.length - 1 } doPlay() })
+function advanceLine() : void {
+  if(!sceneInitialized) {
+    sceneInitialized = true
+  }
+  else if(typeof woyzeck[sceneIndex].outline[outlineIndex] === 'string') {
+    advanceOutline()
+  }
+  else {
+    lineIndex++
+    const d = woyzeck[sceneIndex].outline[outlineIndex] as Dialog
+    if(lineIndex >= d.linesAndDirections.length) {
+      advanceOutline()
+    }
+  }
+}
+
+function advanceOutline() : void {
+  outlineIndex++
+  lineIndex = 0
+  if(outlineIndex >= woyzeck[sceneIndex].outline.length) {
+    advanceScene()
+  }
+}
+
+function advanceScene() : void {
+  sceneIndex++
+  if(sceneIndex >= woyzeck.length) {
+    sceneIndex = 0
+  }
+  sceneInitialized = false
+  outlineIndex = 0
+  lineIndex = 0
+}
+
+function resetPlay() : void {
+  sceneInitialized = false
+  sceneIndex = 0
+  outlineIndex = 0
+  lineIndex = 0
+}
+
+function renderCurrent() : void {
+  if(!sceneInitialized) {
+    speakerContainer.style.display = 'none'
+    lineContainer.style.display = 'none'
+
+    sceneTitleContainer.style.display = 'inherit'
+    const [sceneTitleName, sceneTitleNumeral] = woyzeck[sceneIndex].name.split(' ')
+    sceneTitleContainer.querySelector('.scene-title-name-word').textContent = sceneTitleName
+    sceneTitleContainer.querySelector('.scene-title-name-numeral').textContent = sceneTitleNumeral
+    return
+  }
+
+  sceneTitleContainer.style.display = 'none'
+
+  const { outline } = woyzeck[sceneIndex]
+  const currentEl = outline[outlineIndex]
+
+  if(typeof currentEl === 'string') {
+    // If we have a stage direction, hide the speaker  
+    speakerContainer.style.display = 'none'
+    lineContainer.style.display = 'inherit'
+    lineContainer.textContent = currentEl
+    return
+  }
+
+  const { speaker, linesAndDirections } = currentEl as Dialog
+
+  speakerContainer.style.display = 'inherit'
+  lineContainer.style.display = 'inherit'
+  speakerContainer.textContent = speaker
+  lineContainer.textContent = linesAndDirections[lineIndex]
+}
+
+key('right', () => { advanceLine(); renderCurrent(); })
 
 function populateVoiceList() : void {
   allVoices = speechSynthesis.getVoices()
-  console.log(allVoices.map(v => v.name).sort().join(', '))
-  candidateVoices = allVoices.filter(v => v.name.startsWith('english'))
+  // candidateVoices = allVoices.filter(v => v.name.startsWith('english'))
+  candidateVoices = allVoices.filter(v => v.lang.startsWith('en') || v.lang.startsWith('de'))
+  // console.log(allVoices.map(v => v.name).sort().join(', '))
+  console.log(candidateVoices.map(v => v.name).sort().join(', '))
   usedVoices = []
   if(candidateVoices.length > 0) {
     doPlay()
--- public/index.js
+++ public/index.js
@@ -9,101 +9,26 @@ woyzeck.forEach(function (scene) {
     ds.forEach(function (el) { return characters.add(el.speaker); });
 });
 var preferredVoices = {
-    'Woyzeck': undefined,
-    'Marie': undefined,
-    'Doctor': undefined,
-    'Captain': undefined,
-    'Andres': undefined,
-    'Margaret': undefined,
-    'Drum Major': undefined
+    'Woyzeck': [{ voice: 'english-us', pitch: 1, rate: 1 }],
+    'Marie': [{ voice: 'english', pitch: 1.25, rate: 1 }],
+    'Doctor': [{ voice: 'english_rp', pitch: 0.9, rate: 0.95 }],
+    'Captain': [{ voice: 'en-scottish', pitch: 1, rate: 1 }],
+    'Andres': [{ voice: 'english-north', pitch: 1, rate: 1 }],
+    'Margaret': [{ voice: 'en-westindies', pitch: 1, rate: 1 }],
+    'Drum Major': [{ voice: 'english_wmids', pitch: 1, rate: 1 }]
 };
-var firefoxCandidateVoices = [
-    'english',
-    'english-us',
-    'english_rp',
-    'english-north',
-    'english_wmids',
-    'en-scottish',
-    'en-westindies',
-    'german',
-    'default'
-];
-var firefoxOtherVoices = [
-    'Farsi',
-    'Farsi-Pinglish',
-    'Mandarin',
-    'afrikaans',
-    'albanian',
-    'aragonese',
-    'armenian',
-    'armenian-west',
-    'bosnian',
-    'brazil',
-    'bulgarian',
-    'cantonese',
-    'catalan',
-    'croatian',
-    'czech',
-    'danish',
-    'dutch',
-    'esperanto',
-    'estonian',
-    'finnish',
-    'french',
-    'french-Belgium',
-    'georgian',
-    'greek',
-    'greek-ancient',
-    'hindi',
-    'hungarian',
-    'icelandic',
-    'indonesian',
-    'irish-gaeilge',
-    'italian',
-    'kannada',
-    'kurdish',
-    'latin',
-    'latvian',
-    'lithuanian',
-    'lojban',
-    'macedonian',
-    'malay',
-    'malayalam',
-    'nepali',
-    'norwegian',
-    'polish',
-    'portugal',
-    'punjabi',
-    'romanian',
-    'russian',
-    'serbian',
-    'slovak',
-    'spanish',
-    'spanish-latin-am',
-    'swahili-test',
-    'swedish',
-    'tamil',
-    'turkish',
-    'vietnam',
-    'vietnam_hue',
-    'vietnam_sgn',
-    'welsh'
-];
+var sceneTitleContainer = document.querySelector('#scene-title');
 var speakerContainer = document.querySelector('#speaker');
 var lineContainer = document.querySelector('#line');
+sceneTitleContainer.style.display = 'none';
+speakerContainer.style.display = 'none';
+lineContainer.style.display = 'none';
 var sceneIndex = 0;
+var sceneInitialized = false;
 var outlineIndex = 0;
 var lineIndex = 0;
 function doPlay() {
-    var outline = woyzeck[sceneIndex].outline;
-    var currentEl = outline[outlineIndex];
-    if (typeof currentEl === 'string') {
-        lineContainer.textContent = currentEl;
-        return;
-    }
-    var _a = currentEl, speaker = _a.speaker, linesAndDirections = _a.linesAndDirections;
-    speakerContainer.textContent = speaker;
-    lineContainer.textContent = linesAndDirections[lineIndex];
+    renderCurrent();
     // let voice : SpeechSynthesisVoice
     // if(speaker in charactersToVoices) {
     //   voice = charactersToVoices[speaker]
@@ -128,12 +53,76 @@ function doPlay() {
     // }, 300)
     // speechSynthesis.speak(utterance)
 }
-// key('right', () => { i += 1; if(i === woyzeck.length) { i = 0 } doPlay() })
-// key('left', () => { i -= 1; if(i < 0) { i = woyzeck.length - 1 } doPlay() })
+function advanceLine() {
+    if (!sceneInitialized) {
+        sceneInitialized = true;
+    }
+    else if (typeof woyzeck[sceneIndex].outline[outlineIndex] === 'string') {
+        advanceOutline();
+    }
+    else {
+        lineIndex++;
+        var d = woyzeck[sceneIndex].outline[outlineIndex];
+        if (lineIndex >= d.linesAndDirections.length) {
+            advanceOutline();
+        }
+    }
+}
+function advanceOutline() {
+    outlineIndex++;
+    lineIndex = 0;
+    if (outlineIndex >= woyzeck[sceneIndex].outline.length) {
+        advanceScene();
+    }
+}
+function advanceScene() {
+    sceneIndex++;
+    if (sceneIndex >= woyzeck.length) {
+        sceneIndex = 0;
+    }
+    sceneInitialized = false;
+    outlineIndex = 0;
+    lineIndex = 0;
+}
+function resetPlay() {
+    sceneInitialized = false;
+    sceneIndex = 0;
+    outlineIndex = 0;
+    lineIndex = 0;
+}
+function renderCurrent() {
+    if (!sceneInitialized) {
+        speakerContainer.style.display = 'none';
+        lineContainer.style.display = 'none';
+        sceneTitleContainer.style.display = 'inherit';
+        var _a = woyzeck[sceneIndex].name.split(' '), sceneTitleName = _a[0], sceneTitleNumeral = _a[1];
+        sceneTitleContainer.querySelector('.scene-title-name-word').textContent = sceneTitleName;
+        sceneTitleContainer.querySelector('.scene-title-name-numeral').textContent = sceneTitleNumeral;
+        return;
+    }
+    sceneTitleContainer.style.display = 'none';
+    var outline = woyzeck[sceneIndex].outline;
+    var currentEl = outline[outlineIndex];
+    if (typeof currentEl === 'string') {
+        // If we have a stage direction, hide the speaker  
+        speakerContainer.style.display = 'none';
+        lineContainer.style.display = 'inherit';
+        lineContainer.textContent = currentEl;
+        return;
+    }
+    var _b = currentEl, speaker = _b.speaker, linesAndDirections = _b.linesAndDirections;
+    speakerContainer.style.display = 'inherit';
+    lineContainer.style.display = 'inherit';
+    speakerContainer.textContent = speaker;
+    lineContainer.textContent = linesAndDirections[lineIndex];
+}
+key('right', function () { advanceLine(); renderCurrent(); });
 function populateVoiceList() {
     allVoices = speechSynthesis.getVoices();
-    console.log(allVoices.map(function (v) { return v.name; }).sort().join(', '));
-    candidateVoices = allVoices.filter(function (v) { return v.name.startsWith('english'); });
+    // candidateVoices = allVoices.filter(v => v.name.startsWith('english'))
+    candidateVoices = allVoices.filter(function (v) { return v.lang.startsWith('en') || v.lang.startsWith('de'); });
+    // console.log(allVoices.map(v => v.name).sort().join(', '))
+    console.log(candidateVoices.map(function (v) { return v.name; }).sort().join(', '));
     usedVoices = [];
     if (candidateVoices.length > 0) {
         doPlay();
